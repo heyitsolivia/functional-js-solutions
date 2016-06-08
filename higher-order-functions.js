2b4fe569e54b5d9e0b31cc86981ef3ef9906c7b8
Olivia Cheng <olivia@heyitsolivia.com>
2016-06-08 00:13:52 -0400

    solution to higher order function exercise

---
+++ higher-order-functions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function repeat (operation, num) {
+  if (num == 0) {
+    return;
+  }
+  operation();
+  repeat(operation, num - 1);
+}
+
+// Do not remove the line below
+module.exports = repeat
